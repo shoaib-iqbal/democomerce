c543a6c1e594d4f17591911fe7086da661a18bf0
uzair <uzair@gems.techverx.com>
2016-08-16 19:39:32 +0500

    admin gui working

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +22,7 @@ module DemoStore
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    #config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets','fonts')
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
