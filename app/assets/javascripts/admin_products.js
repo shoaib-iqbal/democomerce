ac847b90d869948388c56f93966554194377c63d
uzair <uzair@gems.techverx.com>
2016-08-18 14:50:51 +0500

    sa

---
+++ app/assets/javascripts/admin_products.js
@@ -0,0 +1,14 @@
+function fetch_specfic_vendor_items(path,id)
+{
+
+	$.ajax({
+	 url: path,
+     data: {"user_id" : id},
+      type: "get",
+      format : 'js',
+     success: function() {
+
+      
+     }
+   });
+}
