0c0a74352a5e6125183823478af538fa3e1de116
= <=>
2016-08-30 15:32:28 +0500

    dynamic symbol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,11 @@ def currency_codes
     end
     currencies
   end
+  def store_unit
+  	unit = Store.first.currency rescue 'USD'
+  	Money::Currency.new(unit).symbol
+
+  end
   def show_price(price)
   	unit = Store.first.currency rescue 'USD'
     return Money.new(price*100, unit).format rescue 0
