9e205184f2a0071019f2cea1b1ab551408588e9c
uzair <uzair@gems.techverx.com>
2016-08-31 18:26:33 +0500

    front end changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,7 +36,8 @@ class ProductsController < ApplicationController
       @colors = Admin::Color.all.where(:user_id => params[:vendor])
     else
       products = Admin::Product.all
-      @products = Admin::Product.filter_search(params, products).page(params[:page]).per(5)
+      @total_products = Admin::Product.filter_search(params, products)
+      @products = @total_products.page(params[:page]).per(5)
       
       @sizes = Admin::Size.all
       @colors = Admin::Color.all
